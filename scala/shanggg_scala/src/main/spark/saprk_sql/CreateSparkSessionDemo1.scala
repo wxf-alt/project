baf9219953a699408de1336370bd8b2d370ba1c5
wxf <wxf18845574579@163.com>
2022-11-16 15:21:06 +0800

    测试功能1

--- scala/shanggg_scala/src/main/spark/saprk_sql/CreateSparkSessionDemo1.scala
+++ scala/shanggg_scala/src/main/spark/saprk_sql/CreateSparkSessionDemo1.scala
@@ -19,8 +19,10 @@ object CreateSparkSessionDemo1 {
     properties.setProperty("user", "root")
     properties.setProperty("password", "root")
 
-    val mySqlDF1: DataFrame = sparkSession.read.jdbc("jdbc:mysql://localhost:3306/db", "student1", properties).repartition(5).select($"s_id",$"s_name",$"s_sex")
-    val mySqlDF2: DataFrame = sparkSession.read.jdbc("jdbc:mysql://localhost:3306/db", "student2", properties).repartition(2).select($"s_id",$"s_name",$"s_sex")
+    val mySqlDF1: DataFrame = sparkSession.read.jdbc("jdbc:mysql://localhost:3306/db", "student1", properties).repartition(5).select($"s_id", $"s_name", $"s_sex")
+    val mySqlDF2: DataFrame = sparkSession.read.jdbc("jdbc:mysql://localhost:3306/db", "student2", properties).repartition(2).select($"s_id", $"s_name", $"s_sex")
+    println(s"并行度：${mySqlDF1.rdd.getNumPartitions}")
+    println(s"并行度：${mySqlDF2.rdd.getNumPartitions}")
 
     val seqJoin: DataFrame = mySqlDF1.as("df1")
       .join(mySqlDF2.as("df2"), Seq("s_id"), "left_outer")
@@ -40,6 +42,10 @@ object CreateSparkSessionDemo1 {
       .join(mySqlDF2.as("df2"), $"df1.s_id" === $"df2.s_id")
     equalInnerJoin.printSchema()
 
+    equalInnerJoin.show()
+    println(s"并行度：${equalInnerJoin.rdd.getNumPartitions}")
+
+    Thread.sleep(100000000)
 
 
 
