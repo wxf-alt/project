605b2ca34281434c944d4fc09f440edf09b64bdd
wxf <wxf18845574579@163.com>
2022-11-21 09:22:22 +0800

    测试 笛卡尔积 连接

--- scala/shanggg_scala/src/main/spark/saprk_sql/SparkSessionTest.scala
+++ scala/shanggg_scala/src/main/spark/saprk_sql/SparkSessionTest.scala
@@ -17,29 +17,33 @@ object SparkSessionTest {
     val sparkSession: SparkSession = SparkSession.builder().appName("SparkSessionTest").config(conf).master("local[*]").getOrCreate()
 
     val properties: Properties = new Properties()
-    properties.setProperty("user","root")
-    properties.setProperty("password","root")
+    properties.setProperty("user", "root")
+    properties.setProperty("password", "root")
 
     val mySqlDF: DataFrame = sparkSession.read.jdbc("jdbc:mysql://localhost:3306/db", "student2", properties)
-      .repartition(64)
     val mySqlDF1: DataFrame = sparkSession.read.jdbc("jdbc:mysql://localhost:3306/db", "student1", properties)
-      .repartition(64)
 
-    val dataFrame: DataFrame = mySqlDF.join(mySqlDF1,Seq("s_id"),"left_outer")
-    println("join 后的分区数：" + dataFrame.rdd.getNumPartitions)
+    // 笛卡尔积
+//    val dataFrame: DataFrame = mySqlDF.join(mySqlDF1,Seq(""),"cross")
+    val dataFrame: DataFrame = mySqlDF.crossJoin(mySqlDF1)
+    println(dataFrame.count())
 
-//    println("mySqlDF Partition:" + mySqlDF.rdd.getNumPartitions)
-//    println("mySqlDF1 Partition:" + mySqlDF1.rdd.getNumPartitions)
-//
-//    val mySqlDFS: Dataset[Row] = mySqlDF.union(mySqlDF1)
-//    println("mySqlDFS Partition:" + mySqlDFS.rdd.getNumPartitions)
-//
-//    val mySqlDFS1: Dataset[Row] = mySqlDFS.repartition(128)
-//    println("mySqlDFS1 Partition   1:" + mySqlDFS1.rdd.getNumPartitions)
 
-    dataFrame.show(20)
+    //    val dataFrame: DataFrame = mySqlDF.join(mySqlDF1,Seq("s_id"),"left_outer")
+    //    dataFrame.show(20)
+    //    println("join 后的分区数：" + dataFrame.rdd.getNumPartitions)
 
-    Thread.sleep(3600000)
+    //    println("mySqlDF Partition:" + mySqlDF.rdd.getNumPartitions)
+    //    println("mySqlDF1 Partition:" + mySqlDF1.rdd.getNumPartitions)
+    //
+    //    val mySqlDFS: Dataset[Row] = mySqlDF.union(mySqlDF1)
+    //    println("mySqlDFS Partition:" + mySqlDFS.rdd.getNumPartitions)
+    //
+    //    val mySqlDFS1: Dataset[Row] = mySqlDFS.repartition(128)
+    //    println("mySqlDFS1 Partition   1:" + mySqlDFS1.rdd.getNumPartitions)
+
+
+    //    Thread.sleep(3600000)
 
   }
 }
